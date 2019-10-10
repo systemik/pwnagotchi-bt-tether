163e1c78db535fc587c59b4e299b1ad7de2117a3
systemik <sebastien2@laverriere.org>
2019-10-10 20:26:57 +0200

    Add routing commands

--- tether-restart.sh
+++ tether-restart.sh
@@ -8,4 +8,10 @@ status=$?
 if [ $status -ne 0 ]; then
 	echo "Connecting to bluetooth"
 	sudo systemctl restart btnap
+
+	sleep 10
+	sudo ifconfig bnep0 192.168.44.100 netmask 255.255.255.0
+	sudo sed -i "s@127.0.0.1@8.8.8.8@g" /etc/resolv.conf
+	sudo route del -net 0.0.0.0 netmask 0.0.0.0 gw 10.0.0.1
+	sudo route add -net 0.0.0.0 netmask 0.0.0.0 gw 192.168.44.1
 fi
